2f8295a687c87768493621b86a96d1736063f9be
Keano Ricketts <keano.ricketts@yahoo.com>
2024-11-19 16:16:58 +0000

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+function App() {
+
+}
+
+createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+)
+
+export default App

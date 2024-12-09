c83277e18ec24afee8f90d8504a930822b65d759
Keano Ricketts <keano.ricketts@yahoo.com>
2024-12-09 17:10:12 +0000

    Added some code to the app.js for later use.

---
+++ app.js
@@ -0,0 +1,8 @@
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+
+searchForm.addEventListener("submit",(event) =>{
+    event.preventDefault()
+
+})
+

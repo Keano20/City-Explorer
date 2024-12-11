edb4b23facef97a7f4556ebda5d66846a950265c
Keano Ricketts <keano.ricketts@yahoo.com>
2024-12-11 18:20:35 +0000

    Started adding functions to the application to handle and display the API data.

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
+//const searchButton= document.getElementById('search-button')
 
-searchForm.addEventListener("submit",(event) =>{
-    event.preventDefault()
+searchForm.addEventListener("submit", async(event) =>{
+    event.preventDefault();
 
+    const city = searchInput.value;
+
+    if(city){
+        try{
+            const cityData = await getCityData(city);
+            await displayCityData(cityData);
+        }
+        catch (error){
+            console.error(error);
+            displayError(error);
+        }
+
+    }
+    else{
+        displayError("Please enter a city")
+    }
 })
+
+async function getCityData(city){
+
+    }
+async function displayCityData(data){
+
+}
+
+function displayError(message){
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+}
+
 

900d5e67f5459bfd2fcfe2d325bf82937aede1b9
Keano Ricketts <keano.ricketts@yahoo.com>
2024-12-16 14:31:01 +0000

    Changes to the structure of the API call file.

--- apiCall.js
+++ apiCall.js
@@ -1,16 +1,7 @@
-export async function apiCall() {
-    const apiUrl = "https://api.api-ninjas.com/v1/city";
-    try {
-        const response = await fetch(apiUrl);
-        console.log(response);
-        if (!response.ok) {
-            console.error(`Response status: ${response.status}`);
-        }
+export async function apiCall(country) {
+    const apiUrl = "https://restcountries.com/v3.1/name/"+country;
+    return await fetch(apiUrl).then(async res=> await res.json())
+         .catch(err=>console.error(err));
 
-        const json = await response.json();
-        console.log(json);
-    } catch (error) {
-        console.error(error.message);
-    }
 }
 

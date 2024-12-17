0c92edbba5f77a34e3807de04490da86f6e663b0
Keano Ricketts <keano.ricketts@yahoo.com>
2024-12-17 20:14:24 +0000

    Added Parameters to the API call.

--- apiCall.js
+++ apiCall.js
@@ -1,6 +1,18 @@
-export async function apiCall(country) {
-    const apiUrl = "https://restcountries.com/v3.1/name/"+country;
-    return await fetch(apiUrl).then(async res=> await res.json())
-        .catch(err=>console.error(err));
-}
+export async function apiCall(city) {
+    const apiUrl = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${city}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': '',
+            'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+        }
+    };
 
+    try {
+        const response = await fetch(apiUrl, options);
+        return await response.json();
+    } catch (error) {
+        console.error("API call failed:", error);
+        throw error;
+    }
+}

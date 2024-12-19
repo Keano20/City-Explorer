987c5a6d27b0a1c0dfd1c7401318051e908c9385
Keano Ricketts <keano.ricketts@yahoo.com>
2024-12-19 21:15:23 +0000

    Added a second API for weather functionality.

--- apiCall.js
+++ apiCall.js
@@ -1,6 +1,8 @@
 export async function apiCall(city) {
     const cityApiUrl = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${city}`;
-    const cityApiOptions = {
+    const weatherApiUrl = `https://weatherapi-com.p.rapidapi.com/current.json?q=${city}`;
+
+    const optionsForCityApi = {
         method: 'GET',
         headers: {
             'x-rapidapi-key': '',
@@ -8,9 +10,29 @@ export async function apiCall(city) {
         }
     };
 
+    const optionsForWeatherApi = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': '',
+            'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
+        }
+    }
+
     try {
-        const response = await fetch(cityApiUrl, cityApiOptions);
-        return await response.json();
+        const [cityResponse, weatherResponse] = await Promise.all([
+            fetch(cityApiUrl, optionsForCityApi),
+            fetch(weatherApiUrl, optionsForWeatherApi)
+        ]);
+
+        const cityData = await cityResponse.json();
+        const weatherData = await weatherResponse.json();
+
+        // I'm logging this for debugging reasons.
+        console.log("City Data:", cityData);
+        console.log("Weather Data:", weatherData);
+
+        return { cityData, weatherData };
+
     } catch (error) {
         console.error("API call failed:", error);
         throw error;
